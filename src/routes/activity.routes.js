8428037eb9e1a548d3c403c76d0d2bda9747ce46
giovanni <ebodegiovanni@gmail.com>
2026-06-23 12:35:19 +0100

    Api binova v1

--- src/routes/activity.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, authorize } = require('../middleware/auth');
-const { getActivities, getActivityStats } = require('../controllers/activity.controller');
-
-router.get('/', protect, authorize('admin', 'super_admin'), getActivities);
-router.get('/stats', protect, authorize('admin', 'super_admin'), getActivityStats);
-
-module.exports = router;
